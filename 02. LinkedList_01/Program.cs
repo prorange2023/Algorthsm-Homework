4ec864ad655814e38a580f6d970caa1bce3efbc5
prorange2023 <prorange2023@gmail.com>
2024-01-12 17:40:45 +0900

    feat : 02. LinkedList과제 01 완료

---
+++ 02. LinkedList_01/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+
+namespace _02._LinkedList_01
+{
+	internal class Program
+	{
+		//사용자의 입력으로 숫자를 입력 받아서(마이너스도 허용)
+		//음수는 앞에 추가하고, 양수는 뒤에 추가하여
+		//음수&양수를 반으로 나누는 연결리스트 구현
+		//입력 받을 때마다 처음부터 끝까지 출력 진행
+		public class Funny
+		{
+			public int listcount = 0;
+			public LinkedList<int>? linkedlist;
+
+			public int InputCount()
+			{
+				Console.WriteLine("만들 node의 수를 입력하세요");
+				int count = int.Parse(Console.ReadLine());
+				return count;
+			}
+
+
+			public void MakeLinkedList(int num)
+			{
+				for (int i = 0; i < num; i++)
+				{
+					InputNode();
+					PrintValue();
+				}
+
+			}
+			public void InputNode()
+			{
+				Console.WriteLine("정수를 입력하세요");
+				int num = int.Parse(Console.ReadLine());
+
+				int value = 0;
+
+				if (num < 0)
+				{
+					linkedlist.AddFirst(num);
+					listcount++;
+				}
+				else if (num > 0)
+				{
+					linkedlist.AddLast(num);
+					listcount++;
+				}
+				else
+				{
+					Console.WriteLine("0은 제외하고 입력해주세요");
+				}
+
+				PrintValue();
+			}
+
+			public void PrintValue()
+			{
+				foreach (int value in linkedlist)
+				{
+
+					if (linkedlist == null)
+					{
+						Console.WriteLine("값이 비어있습니다.");
+					}
+					else 
+					{
+						Console.WriteLine($"{value}"); 
+					}
+					
+				}
+			}
+		}
+		static void Main(string[] args)
+		{
+			LinkedList<int> linkedlist = new LinkedList<int>();
+			Funny funny = new Funny();
+			int count = funny.InputCount();
+			
+			for (int i = 0; funny.listcount < count; i++)
+			{
+				Console.WriteLine("정수를 입력하세요");
+				int num = int.Parse(Console.ReadLine());
+				
+				if (num < 0)
+				{
+					linkedlist.AddFirst(num);
+					funny.listcount++;
+				}
+				else if (num > 0)
+				{
+					linkedlist.AddLast(num);
+					funny.listcount++;
+				}
+				else
+				{
+					Console.WriteLine("0은 제외하고 입력해주세요");
+				}
+				funny.PrintValue();
+			}
+		}
+	}
+}
---
+++ 02_LinkedList_02/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_LinkedList_02
+{
+	internal class Program
+	{
+		//요세푸스 문제는 다음과 같다.
+		//1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다.이제 순서대로 K번째 사람을 제거한다.
+		//한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다. 이 과정은 N명의 사람이 모두 제거될 때까지 계속된다.
+		//원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다.예를 들어 (7, 3)-요세푸스 순열은<3, 6, 2, 7, 5, 1, 4>이다.
+		//N과 K가 주어지면 (N, K)-요세푸스 순열을 구하는 프로그램을 작성하시오.
+		
+		static void Main(string[] args)
+		{
+			LinkedList<int> linkedlist = new LinkedList<int>();
+			Console.WriteLine("인원수를 입력하세요");
+			int member = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < member; i++)
+			{
+				linkedlist.AddLast(i+1);
+			}
+
+			foreach (int i in linkedlist)
+			{
+				Console.WriteLine(i);
+			}
+
+			LinkedListNode<int> head = linkedlist.First;
+			LinkedListNode<int> tail = linkedlist.Last;
+
+			Console.WriteLine($"{tail.Value}");
+			Console.WriteLine($"{head.Value}");
+
+			tail.Next = head;
+
+			if (tail.Next == null )
+			{
+				tail.next = head;
+			}
+			else
+			{
+
+			}
+
+			
+
+			Console.WriteLine("무작위 숫자를 입력하세요");
+			int deathturn = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < member; i++)
+            {
+				for (int j = 1; j < deathturn; j++)
+				{
+					head = head.Next;
+				}
+
+				Console.WriteLine(head.Value);
+			}
+
+
+
+			
+
+
+		}
+	}
+}
