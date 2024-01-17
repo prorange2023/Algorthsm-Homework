140b6170c9d7a5653014865356957e87fb036ffb
prorange2023 <prorange2023@gmail.com>
2024-01-18 08:59:34 +0900

    05. Heap 과제 추가

---
+++ 05. Heap01/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _05._Heap01
+{
+	public class Problem3
+	{
+		public struct Homework
+		{
+			public string name;
+			public int deadline;
+			public int score;
+
+			public Homework(string name, int deadline, int score)
+			{
+				this.name = name;
+				this.deadline = deadline;
+				this.score = score;
+			}
+		}
+
+		public void Run()
+		{
+			int sumScore = 0;
+			List<Homework> homeworks = new List<Homework>();
+
+			homeworks.Add(new Homework("A", 4, 60));
+			homeworks.Add(new Homework("B", 4, 40));
+			homeworks.Add(new Homework("C", 1, 20));
+			homeworks.Add(new Homework("D", 2, 50));
+			homeworks.Add(new Homework("E", 3, 30));
+			homeworks.Add(new Homework("F", 4, 10));
+			homeworks.Add(new Homework("G", 6, 5));
+
+			// 마감일이 많이 남은 순서대로 꺼내기 위해 내림차순 우선순위 큐에 추가
+			PriorityQueue<Homework, int> remainPQ = new PriorityQueue<Homework, int>();
+			foreach (Homework work in homeworks)
+			{
+				remainPQ.Enqueue(work, -work.deadline);
+			}
+
+			// 뒤에서부터 가장 높은 점수 순서대로 처리
+			PriorityQueue<Homework, int> scorePQ = new PriorityQueue<Homework, int>();
+			for (int day = remainPQ.Peek().deadline; day >= 1; day--)
+			{
+				while (remainPQ.Count > 0 && remainPQ.Peek().deadline == day)
+				{
+					Homework homework = remainPQ.Dequeue();
+					scorePQ.Enqueue(homework, -homework.score);
+				}
+
+				if (scorePQ.Count > 0)
+				{
+					Homework target = scorePQ.Dequeue();
+					sumScore += target.score;
+					Console.WriteLine($"{target.name} 과제를 {day} 일차에 수행해야 한다. 총점 : {sumScore}");
+				}
+
+				if (remainPQ.Count == 0)
+					break;
+			}
+		}
+	}
+
+}
---
+++ 05. Heap02/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _05._Heap02
+{
+	public class Problem4
+	{
+		public struct Meeting
+		{
+			public string name;
+			public int start;
+			public int end;
+
+			public Meeting(string name, int start, int end)
+			{
+				this.name = name;
+				this.start = start;
+				this.end = end;
+			}
+		}
+
+		public void Run()
+		{
+			List<Meeting> meetings = new List<Meeting>();
+
+			meetings.Add(new Meeting("A", 0, 40));
+			meetings.Add(new Meeting("B", 15, 30));
+			meetings.Add(new Meeting("C", 5, 10));
+			meetings.Add(new Meeting("D", 20, 40));
+			meetings.Add(new Meeting("E", 1, 5));
+
+			PriorityQueue<Meeting, int> remainMeeting = new PriorityQueue<Meeting, int>();
+			foreach (Meeting meeting in meetings)
+			{
+				remainMeeting.Enqueue(meeting, meeting.start);
+			}
+
+			PriorityQueue<Meeting, int> doMeeting = new PriorityQueue<Meeting, int>();
+			while (remainMeeting.Count > 0)
+			{
+				Meeting nextStartMeeting = remainMeeting.Dequeue();
+				if (doMeeting.Count == 0)
+				{
+					Console.WriteLine($"{nextStartMeeting.name}회의를 열기에 비어있는 회의실이 없습니다.");
+					doMeeting.Enqueue(nextStartMeeting, nextStartMeeting.end);
+					Console.WriteLine($"회의실을 추가로 확보하여 {doMeeting.Count}개의 회의실을 사용합니다.");
+				}
+				else
+				{
+					Meeting nextEndMeeting = doMeeting.Peek();
+					if (nextEndMeeting.end <= nextStartMeeting.start)
+					{
+						Console.WriteLine($"{nextEndMeeting.name}이 끝난 회의실에 {nextStartMeeting.name}이 시작합니다.");
+						doMeeting.Dequeue();
+						doMeeting.Enqueue(nextStartMeeting, nextStartMeeting.end);
+					}
+					else
+					{
+						Console.WriteLine($"{nextStartMeeting.name}회의를 열기에 비어있는 회의실이 없습니다.");
+						doMeeting.Enqueue(nextStartMeeting, nextStartMeeting.end);
+						Console.WriteLine($"회의실을 추가로 확보하여 {doMeeting.Count}개의 회의실을 사용합니다.");
+					}
+				}
+			}
+
+			Console.WriteLine($"필요한 회의실의 갯수는 {doMeeting.Count}개 입니다");
+		}
+	}
+
+}
